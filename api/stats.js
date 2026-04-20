0a7f67242bab7e6b016356bfeb3468c6ca121bb3
nicojaks <147146511+nicojaks@users.noreply.github.com>
2026-04-21 00:13:01 +0200

    Create stats.js

---
+++ api/stats.js
@@ -0,0 +1,57 @@
+export default async function handler(req, res) {
+
+  const players = [
+    "rehan2109",
+    "Nivs",
+    "Chr1sser",
+    "DeeQzy",
+    "Sunlollyice",
+    "Ska1ps"
+  ];
+
+  const result = {};
+
+  for (const name of players) {
+
+    const player = await fetchPlayer(name);
+    const matches = await fetchMatches(player.player_id);
+
+    matches.slice(0, 20).forEach(m => {
+      const map = m.game_mode || "unknown";
+
+      const key = `${name}-${map}`;
+      result[key] = (result[key] || 0) + 1;
+    });
+  }
+
+  res.status(200).json(result);
+}
+
+/* ===== FACEIT API ===== */
+
+async function fetchPlayer(name) {
+  const res = await fetch(
+    `https://open.faceit.com/data/v4/players?nickname=${name}`,
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.FACEIT_KEY}`
+      }
+    }
+  );
+
+  return res.json();
+}
+
+async function fetchMatches(id) {
+  const res = await fetch(
+    `https://open.faceit.com/data/v4/players/${id}/history?game=cs2&limit=20`,
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.FACEIT_KEY}`
+      }
+    }
+  );
+
+  const data = await res.json();
+  return data.items || [];
+}
